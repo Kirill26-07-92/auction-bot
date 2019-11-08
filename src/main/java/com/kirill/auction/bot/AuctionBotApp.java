d509a3eb4fa530960364f21bd90518fe9692cb60
kirill26-07-92 <8080rfvhu!>
2019-11-08 13:56:44 +0300

    Add controllers for bot management

--- src/main/java/com/kirill/auction/bot/AuctionBotApp.java
+++ src/main/java/com/kirill/auction/bot/AuctionBotApp.java
@@ -1,4 +1,13 @@
 package com.kirill.auction.bot;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class AuctionBotApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionBotApp.class, args);
+    }
+
 }
