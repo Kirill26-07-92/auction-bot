873be26eae6eab8e90043a7ce04a039a3848d963
kirill26-07-92 <8080rfvhu!>
2019-11-08 15:01:16 +0300

    [request_model] - add InitializationRequest model

--- src/main/java/com/kirill/auction/bot/model/InitializationRequest.java
+++ src/main/java/com/kirill/auction/bot/model/InitializationRequest.java
@@ -1,4 +1,44 @@
 package com.kirill.auction.bot.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import java.util.Objects;
+
+@ApiModel("The production quantity and the allowed cash limit for a bidder initialization")
 public class InitializationRequest {
+
+    @ApiModelProperty("The quantity units")
+    private int quantity;
+
+    @ApiModelProperty("The cash limit")
+    private int cash;
+
+    public InitializationRequest(final int quantity, final int cash) {
+        this.quantity = quantity;
+        this.cash = cash;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getCash() {
+        return cash;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof InitializationRequest)) return false;
+        InitializationRequest request = (InitializationRequest) o;
+        return quantity == request.quantity &&
+                cash == request.cash;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(quantity, cash);
+    }
+
 }
